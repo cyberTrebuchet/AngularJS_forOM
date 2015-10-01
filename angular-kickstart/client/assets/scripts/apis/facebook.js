6d5d4308974135a38776aa7871c3e3b96f783a19
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-01 16:23:43 -0400

    update facebook.js

--- angular-kickstart/client/assets/scripts/apis/facebook.js
+++ angular-kickstart/client/assets/scripts/apis/facebook.js
@@ -32,7 +32,7 @@ function checkLoginState() {
 
 window.fbAsyncInit = function() {
   FB.init({
-    appId      : '681416328660384',
+    appId      : ENV[Facebook_ID],
     cookie     : true,  // enable cookies to allow the server to access
                         // the session
     xfbml      : true,  // parse social plugins on this page
